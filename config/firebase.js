d48f217ed9de552d88193623cb290fed5250dc1b
Manasa <manasakm549@outlook.com>
2025-11-30 23:48:16 +0530

    Fix firebase credential loading for Render

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,9 @@
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  });
-}
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 export default admin;
